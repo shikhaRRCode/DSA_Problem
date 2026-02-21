de20500ead3bc7a72bb21b2f3915961f7b439662
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-02-21 23:56:37 +0530

    Time: 8 ms (45.19%) | Memory: 42.3 MB (63.76%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -1,6 +1,8 @@
 class Solution {
     public int countPrimeSetBits(int left, int right) 
     {
+        // 2^10 --> 1024 --> 10^3 (approx)
+        //10^6 --> 2^20  --> worst case 20 set-bits(so, store prime no. b/w 1-20)
         HashSet<Integer> set = new HashSet<>(Set.of(2,3,5,7,11,13,17,19));
         int count = 0;
         for(int i = left ; i <= right ; i++)
