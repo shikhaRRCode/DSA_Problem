72761f5af37dbf3320a28978ad15774111e94cfc
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-02-22 23:16:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (48.75%) - LeetSync

--- 899-binary-gap/binary-gap.java
+++ 899-binary-gap/binary-gap.java
@@ -5,15 +5,18 @@ class Solution {
         int currbit = 0;
         while(n > 0)
         {
+            // Check if current bit is 1
             if(n%2 == 1){
+                // If we have seen a previous 1 before
                 if(prevbit != -1)
                 {
                     ans = Math.max(ans , currbit - prevbit);
                 }
+                // Update previous 1 position to current position
                 prevbit = currbit;
             }
             currbit++;
-            n = n >> 1;    //Right Shift
+            n = n >> 1;    // Right shift n (removes last bit)
         }
         return ans;
     }
