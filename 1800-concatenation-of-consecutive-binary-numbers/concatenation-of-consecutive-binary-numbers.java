1d87d89cd72087ef94e063511352100481573554
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-02-28 20:29:42 +0530

    Time: 24 ms (98.84%) | Memory: 42.6 MB (50.00%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
@@ -3,10 +3,15 @@ class Solution {
     {
         long result = 0;
         int MOD = 1000000007;          // used to prevent overflow
+        int digits = 0;
         for(int num = 1 ; num <= n ; num++)
         {
-            // find number of binary digits in current number
-            int digits = (int)(Math.log(num)/Math.log(2)) + 1;
+            //Finding whether a number is power of 2 or not
+            //beacuse at power 2 , no. of digits to represent number in binary increases by 1
+            if((num & (num-1)) == 0)
+            {
+                digits++;
+            }
 
             // shift result left to make space and add current number
             result = ((result << digits) + num) % MOD;
