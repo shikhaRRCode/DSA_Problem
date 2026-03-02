e77b8c6189f24d706e9334b3623570fe06577158
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-03-02 23:39:21 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (21.95%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double myPow(double x, int n) 
+    {
+        return solve(x , n);
+    }
+    public double solve(double x, long n){
+        if(n == 0)  return 1;            // anything power 0 = 1
+        
+        long pow = n;       //convert to long to handle overflow(as pow=-2^31 ko +ve krenge to overflow)
+
+        if(pow < 0){
+           return solve(1/x , -pow);
+        }
+
+        if(n % 2 == 0) return solve(x*x , pow/2);
+
+        else return x * solve(x*x , pow/2);
+    }
+}
+// Binary Exponentiation : 2^8 --> (2^2)^4
+// pow:even --> pow(x*x , n/2)
+// pow:odd --> x + pow(x*x , (n-1)/2)
