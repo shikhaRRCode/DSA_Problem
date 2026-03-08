633fe4493ff5f36d377cc9226c5ce2120e2c6d52
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-03-08 22:33:35 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (91.35%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.java
+++ 3952-trionic-array-i/trionic-array-i.java
@@ -4,13 +4,15 @@ class Solution {
         int n = nums.length;
         int count = 0;
         for(int i = 0  ; i < n-1 ; i++){
+            // even count → expect increasing phase
             if(count % 2 == 0 && nums[i] < nums[i+1]){
                 count++;
                 while(i < n-1 && nums[i] < nums[i+1]){
                     i++;
                 }
-                i--;
+                i--;          // adjust index because for-loop will increment
             }
+            // odd count → expect decreasing phase
             else if(count % 2 == 1 && nums[i] > nums[i+1]){
                 count++;
                 while(i < n-1 && nums[i] > nums[i+1]){
@@ -18,10 +20,12 @@ class Solution {
                 }
                 i--;
             }
+            // if pattern breaks, array is not trionic
             else{
                 return false;
             }
         } 
+        // valid only if we saw exactly 3 phases: increase → decrease → increase
         return count == 3;   
     }
 }
