50aae1c47c0f9d8190731910a6858763092f2d25
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-03-11 23:20:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (15.71%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -2,14 +2,12 @@ class Solution {
     public int bitwiseComplement(int n) {
         if(n == 0) return 1;
 
-        int mask = 1;
-        while(mask < n){
-            //leftShift Mask to make space for addition
-            mask =  mask << 1;
-            //add (or operator)
-            mask = mask | 1;
-        }
-        //Xor operation : Ex : 101 ^ 111 = 010
-        return n ^  mask;
-    }                          //T.C = O(log n)
+        // find number of bits in n using log base 2
+        int bits = (int)Math.floor(Math.log(n) / Math.log(2)) + 1;
+
+        int mask = (1 << bits) - 1;  //Math.pow(2 , bits) - 1 = 11....1;
+        
+        // XOR with mask flips all bits
+        return n ^ mask;
+    }                          //T.C = O(1)
 }
