14bc39138072dca6f9f52254e76171150ccab0bc
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-03-15 23:00:08 +0530

    Time: 360 ms (5.39%) | Memory: 48 MB (37.63%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,28 +1,36 @@
 class Solution {
+    // memoization array to store result of each index
     int[] t = new int[10001];
+
     public boolean canJump(int[] nums) {
         int n = nums.length;
+
+        // initialize memo with -1 (not computed yet)
         Arrays.fill(t , -1);
 
         return solve(nums , n , 0);
     }
     public boolean solve(int[] nums , int n , int idx)
     {
+        // if we reach last index → jump is possible
         if(idx == n-1){
             return true;
         }
 
+        // if already solved before, return stored result
         if(t[idx] != -1){
             return t[idx] == 0 ? false : true;
         }
 
+        // try all possible jumps from current index
         for(int i = 1 ; i <= nums[idx] ; i++){
             if(solve(nums , n , idx + i)){
-                t[idx] = 1;
+                t[idx] = 1;    // store true
                 return true;
             }
         }
+        // if no jump works, mark this index as false
         t[idx] = 0;
         return false;
     }
-}
+}                                       
