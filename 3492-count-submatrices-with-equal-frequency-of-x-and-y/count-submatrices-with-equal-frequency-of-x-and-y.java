19de3e055d04b753c8b32a79628c160a9c51e2a5
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-03-19 22:47:29 +0530

    Time: 36 ms (36.54%) | Memory: 249.7 MB (59.62%) - LeetSync

--- 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.java
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.java
@@ -4,6 +4,7 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
 
+        // prefix sum arrays to count X and Y separately
         int[][] cumSumX = new int[n][m];
         int[][] cumSumY = new int[n][m];
 
@@ -12,19 +13,22 @@ class Solution {
         {
             for(int j = 0 ; j < m ; j++)
             {
+                // mark 1 if current cell is X or Y
                 cumSumX[i][j] = (grid[i][j] == 'X') ? 1 : 0;
                 cumSumY[i][j] = (grid[i][j] == 'Y') ? 1 : 0;
 
+                // build prefix sum for X
                 if(i > 0)   cumSumX[i][j] += cumSumX[i-1][j];
                 if(j > 0)   cumSumX[i][j] += cumSumX[i][j-1];
                 if(i > 0 && j > 0)   cumSumX[i][j] -= cumSumX[i-1][j-1];
 
+                // build prefix sum for Y
                 if(i > 0)   cumSumY[i][j] += cumSumY[i-1][j];
                 if(j > 0)   cumSumY[i][j] += cumSumY[i][j-1];
                 if(i > 0 && j > 0)   cumSumY[i][j] -= cumSumY[i-1][j-1];
 
 
-                //an equal frequency of 'X' and 'Y' AND at least one 'X'.
+                //Check : an equal frequency of 'X' and 'Y' AND at least one 'X'.
                 if(cumSumX[i][j] == cumSumY[i][j] && cumSumX[i][j] > 0){
                     count++;
                 }
