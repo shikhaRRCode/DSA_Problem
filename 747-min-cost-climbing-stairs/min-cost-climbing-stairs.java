a7bcb0135fda2118e600e8cee2dbf9b4b549b7d1
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-03-20 23:56:59 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (85.06%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) 
+    {
+        //Bottom-Up approach (Tabulation)
+        int n = cost.length;
+        if(n == 2){
+            return Math.min(cost[0],cost[1]);
+        }
+
+        for(int i = 2 ; i < n ; i++)
+        {
+            cost[i] = cost[i] + Math.min(cost[i-1] , cost[i-2]);
+        }
+
+        return 0 + Math.min(cost[n-1] , cost[n-2]);
+    }
+
+}
