365615d34282718d66e79717195b28d28d563048
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-04-01 21:08:40 +0530

    Time: 44 ms (61.74%) | Memory: 114.8 MB (71.30%) - LeetSync

---
+++ 2846-robot-collisions/robot-collisions.java
@@ -0,0 +1,59 @@
+class Solution {
+    public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
+        int n = positions.length;
+        Integer[] indices = new Integer[n];
+
+        for(int i = 0 ; i < n ; i++){
+            indices[i] = i;
+        }
+        //custom sorting:
+        Arrays.sort(indices , (i,j) -> Integer.compare(positions[i],positions[j]));
+
+
+        Stack<Integer> st = new Stack<>();
+        for(int idx : indices){
+            char currDir = directions.charAt(idx);
+
+            if (currDir == 'R') {
+                st.push(idx);
+            } else {
+                // currDir == 'L'
+                while (!st.isEmpty() && directions.charAt(st.peek()) == 'R') {
+
+                    int top = st.peek();
+
+                    if (healths[top] > healths[idx]) {
+                        healths[top]--;
+                        healths[idx] = 0;
+                        break;
+                    } 
+                    else if (healths[top] == healths[idx]) {
+                        healths[top] = 0;
+                        healths[idx] = 0;
+                        st.pop();
+                        break;
+                    } 
+                    else {
+                        healths[top] = 0;
+                        st.pop();
+                        healths[idx]--;
+
+                        // If current robot still alive, push it
+                        if (st.isEmpty() && healths[idx] > 0) {
+                            st.push(idx);
+                            break;
+                        }
+                    }
+                }
+            }    
+        }
+
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i = 0 ; i < n ; i++){
+            if(healths[i] != 0){
+                ans.add(healths[i]);
+            }
+        }
+        return ans;
+    }
+}
