47b8302e2305ad08516fbfc2273fd9f011c6c33b
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-04-01 22:11:14 +0530

    Time: 37 ms (75.65%) | Memory: 114.4 MB (79.13%) - LeetSync

--- 2846-robot-collisions/robot-collisions.java
+++ 2846-robot-collisions/robot-collisions.java
@@ -3,36 +3,40 @@ class Solution {
         int n = positions.length;
         Integer[] indices = new Integer[n];
 
+        // store indices (to sort based on positions)
         for(int i = 0 ; i < n ; i++){
             indices[i] = i;
         }
-        //custom sorting:
+        //custom sorting: sort robots by position (left → right)
         Arrays.sort(indices , (i,j) -> Integer.compare(positions[i],positions[j]));
 
 
-        Stack<Integer> st = new Stack<>();
+        Stack<Integer> st = new Stack<>();             // stores indices of robots
         for(int idx : indices){
             char currDir = directions.charAt(idx);
 
+            // if moving right → just push
             if (currDir == 'R') {
                 st.push(idx);
-            } else {
-                // currDir == 'L'
-                while (!st.isEmpty() && directions.charAt(st.peek()) == 'R') {
+            } else {             // current robot moving left
+               // fight with previous robots in stack
+                while (!st.isEmpty()) {
 
                     int top = st.peek();
-
+                    // if top robot stronger → current dies
                     if (healths[top] > healths[idx]) {
                         healths[top]--;
                         healths[idx] = 0;
                         break;
                     } 
+                    // equla -> both dies
                     else if (healths[top] == healths[idx]) {
                         healths[top] = 0;
                         healths[idx] = 0;
                         st.pop();
                         break;
                     } 
+                    // current stronger → top dies, continue fighting
                     else {
                         healths[top] = 0;
                         st.pop();
@@ -42,7 +46,7 @@ class Solution {
                 }
             }    
         }
-
+        // collect surviving robots (health > 0)
         ArrayList<Integer> ans = new ArrayList<>();
         for(int i = 0 ; i < n ; i++){
             if(healths[i] != 0){
