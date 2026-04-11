19a0d5dacf0a1660dd8c6efb5e4e079d98b03865
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-04-11 20:43:23 +0530

    Time: 3 ms (45.13%) | Memory: 46.5 MB (67.19%) - LeetSync

---
+++ 1380-number-of-closed-islands/number-of-closed-islands.java
@@ -0,0 +1,50 @@
+class Solution {
+    int[] row = {-1 , 1 , 0 , 0};
+    int[] col = {0 , 0 , -1 , 1};
+    int n , m;
+    public int closedIsland(int[][] grid) {
+        n = grid.length;
+        m = grid[0].length;
+
+        //1. Ignore boundary 0's(land)
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(i == 0 || i == n-1 || j == 0 || j == m-1){
+                    if(grid[i][j] == 0){
+                        DFS(i , j , grid);
+                    }
+                }
+            }
+        }
+
+        int count = 0;
+        //2. counting closed islands
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(grid[i][j] == 0){
+                    count++;
+                    DFS(i , j , grid);
+                }
+            }
+        }
+        return count;
+    }
+    public void DFS(int i ,int j , int[][] grid){
+        if(grid[i][j] == 1 || grid[i][j] == 2){
+            return;
+        }
+
+        grid[i][j] = 2;
+
+        for(int k = 0 ; k < 4 ; k++){
+            int new_i = i + row[k];
+            int new_j = j + col[k];
+            if(valid(new_i , new_j)){
+                DFS(new_i , new_j , grid);
+            }
+        }
+    }
+    public boolean valid(int i , int j){
+        return i >= 0 && i < n && j >= 0 && j < m;
+    }
+}
