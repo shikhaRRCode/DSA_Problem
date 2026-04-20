fbd313df3018a23f1371f16b50db65e2fd372626
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-04-20 20:37:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (80.83%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.java
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.java
@@ -1,9 +1,12 @@
 class Solution {
     public int maxDistance(int[] colors) {
         int n = colors.length;
+        //Greedy Approach: Edge houses have farthest diastance
+        // Idea: max distance will always involve one end (0 or n-1)
 
         int max_Dist = 0;
         int i = 0 , j = n-1;
+        // Case 1: Fix right end, move left pointer
         while(i < j){
             if(colors[i] != colors[j]){
                 max_Dist = Math.max(max_Dist , j - i);
@@ -13,6 +16,7 @@ class Solution {
         }
 
         i = 0 ; j = n-1;
+        // Case 2: Fix left end, move right pointer
         while(j > i){
             if(colors[i] != colors[j]){
                 max_Dist = Math.max(max_Dist , j - i);
