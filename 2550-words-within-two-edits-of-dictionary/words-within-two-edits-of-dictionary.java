54e7aa73d66fbcdf12887aab616a7609772de394
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-04-22 16:54:45 +0530

    Time: 17 ms (16.18%) | Memory: 44.4 MB (50.00%) - LeetSync

---
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+
+        ArrayList<String> ans = new ArrayList<>();
+        for(String que : queries){
+            for(String dict : dictionary){
+                int diff = 0;
+                for(int i = 0 ; i < dict.length() ; i++){
+                    if(que.charAt(i) != dict.charAt(i)){
+                        diff++;
+                    }
+                }
+
+                if(diff <= 2){
+                    ans.add(que);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
