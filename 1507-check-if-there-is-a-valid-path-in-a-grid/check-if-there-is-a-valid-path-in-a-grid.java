e3bdcb232d3404fa7fa389e0d41dba12b3088c8a
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-04-27 12:10:40 +0530

    Time: 17 ms (80.09%) | Memory: 104 MB (30.42%) - LeetSync

--- 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.java
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.java
@@ -1,12 +1,12 @@
 class Solution {
     int[][][] directions = { 
-        {  {}    ,  {}   },
-        { {0,-1} , {0,1} },
-        { {-1,0} , {1,0} },
-        { {0,-1} , {1,0} },
-        { {1,0}  , {0,1} },
-        { {0,-1} , {-1,0} },
-        { {-1,0} , {0,1} }
+        {  {}    ,  {}   },      
+        { {0,-1} , {0,1} },           //1 : left , right
+        { {-1,0} , {1,0} },           //2 : up   , down
+        { {0,-1} , {1,0} },           //3 : left , down
+        { {1,0}  , {0,1} },           //4 : down , right
+        { {0,-1} , {-1,0} },          //5 : left , up
+        { {-1,0} , {0,1} }            //6 : up   , right
         };
     int n , m;    
     public boolean hasValidPath(int[][] grid) {
@@ -14,9 +14,9 @@ class Solution {
         m = grid[0].length;
 
         boolean[][] visited = new boolean[n][m];
-        return DFS(0 , 0 , -1 , -1 , grid , visited);
+        return DFS(0 , 0 , grid , visited);
     }
-    public boolean DFS(int i , int j , int prev_i , int prev_j ,  int[][] grid , boolean[][] visited){
+    public boolean DFS(int i , int j ,  int[][] grid , boolean[][] visited){
         if(i == n-1 && j == m-1){
             return true;
         }
@@ -27,11 +27,11 @@ class Solution {
             int x = i + dir[0];
             int y = j + dir[1];
 
-            if(valid(x , y ) && !visited[x][y]  && (x != prev_i || y != prev_j)){
+            if(valid(x , y ) && !visited[x][y]){
                 boolean goBack = check(x , y , i , j , directions[grid[x][y]]);
 
                 if(goBack){
-                    if(DFS(x , y , i , j , grid , visited)){
+                    if(DFS(x , y , grid , visited)){
                         return true;
                     }
                 }
