ed0e9ac8d57563c5da07a724a54deb907d1d0545
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-04-27 12:16:01 +0530

    Time: 26 ms (48.80%) | Memory: 103.9 MB (30.64%) - LeetSync

--- 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.java
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.java
@@ -1,4 +1,5 @@
 class Solution {
+    // directions for each street type
     int[][][] directions = { 
         {  {}    ,  {}   },      
         { {0,-1} , {0,1} },           //1 : left , right
@@ -17,22 +18,26 @@ class Solution {
         return DFS(0 , 0 , grid , visited);
     }
     public boolean DFS(int i , int j ,  int[][] grid , boolean[][] visited){
+        // if reached end → path found
         if(i == n-1 && j == m-1){
             return true;
         }
 
         visited[i][j] = true;
 
+        // try all allowed directions of current cell
         for(int[] dir : directions[ grid[i][j] ]){
             int x = i + dir[0];
             int y = j + dir[1];
 
             if(valid(x , y ) && !visited[x][y]){
+                // check if next cell connects back
                 boolean goBack = check(x , y , i , j , directions[grid[x][y]]);
 
+                // if connection valid → move forward
                 if(goBack){
                     if(DFS(x , y , grid , visited)){
-                        return true;
+                        return true;         //path found
                     }
                 }
 
@@ -40,14 +45,17 @@ class Solution {
         }
         return false;
     }
+    // check boundaries
     public boolean valid(int i , int j){
         return i >= 0 && i < n && j >= 0 && j < m;
     }
+    // check if next cell connects back to current cell
     public boolean check(int x , int y , int i , int j , int[][] d){
         for(int[] dir : d){
             int new_i =x + dir[0];
             int new_j =y + dir[1];
 
+            // if it can come back → valid connection
             if(new_i == i && new_j == j){
                 return true;
             }
