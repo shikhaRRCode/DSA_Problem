d86bc2a80fc48f29aab75dbcd356af4cd8ca677e
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-05-20 23:00:47 +0530

    Time: 4 ms (53.95%) | Memory: 47.4 MB (51.46%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
@@ -3,14 +3,18 @@ class Solution {
         int n = A.length;
 
         int[] C = new int[n];
+
+        //Elements are in range 1 - n
+        boolean isPresentA[] = new boolean[n+1];
+        boolean isPresentB[] = new boolean[n+1];
+
         for(int i = 0 ; i < n ; i++){
+            isPresentA[A[i]] = true;
+            isPresentB[B[i]] = true;
             int count = 0;
-            for(int j = 0 ; j <= i ; j++){
-                for(int k = 0 ; k <= i ; k++){
-                    if(A[j] == B[k]){
-                        count++;
-                        break;
-                    } 
+            for(int j = 1 ; j <= n ; j++){
+                if(isPresentA[j] && isPresentB[j]){
+                    count++;
                 }
             }
             C[i] = count;
