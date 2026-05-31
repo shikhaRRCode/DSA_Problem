8b94c27afafdfccaba310795764b5fd9a30016a7
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-05-31 19:51:13 +0530

    Time: 24 ms (94.23%) | Memory: 107.7 MB (16.40%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.java
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -4,17 +4,15 @@ class Solution {
         int n = asteroids.length;
         Arrays.sort(asteroids);
 
+        long currmass = mass;
         for(int i  = 0 ; i < n ; i++){
-            if(mass >= asteroids[i]){
-                mass += asteroids[i];
+            if(currmass >= asteroids[i]){
+                currmass += asteroids[i];
             }
             else{
                 return false;
             }
 
-            if(mass >= asteroids[n-1]){
-                break;
-            }
         }
         return true;
     }
