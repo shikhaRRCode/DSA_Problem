ea46d806458c01854fcb1120714ae4ec473c57ee
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-06-04 16:40:11 +0530

    Time: 14 ms (90.71%) | Memory: 42.6 MB (93.31%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
@@ -1,20 +1,24 @@
 class Solution {
     public int totalWaviness(int num1, int num2) {
         int count = 0;
+        // check each number one by one
         for(int i = num1 ; i <= num2 ; i++)
         {
-            int front = -1;
+            int front = -1;                             // front = right digit
             int num = i;
+            // extract digits from right to left
             while(num > 0){
-                int curr = num%10;
+                int curr = num%10;                      // curr = middle digit
                 num/=10;
 
                 if(num != 0 && front != -1){
-                    int prev = num%10;
+                    int prev = num%10;                  // prev = left digit
+                    // curr is a valley 
                     if(prev > curr && curr < front){
                         count++;
                     }
 
+                    // curr is a hill
                     if(prev < curr && curr > front){
                         count++;
                     }
