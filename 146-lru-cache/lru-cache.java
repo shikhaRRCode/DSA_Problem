91ab80119e3484c79199bdfff15be1b46ae1899c
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-06-05 23:42:21 +0530

    Time: 46 ms (63.33%) | Memory: 130.2 MB (94.46%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,4 +1,5 @@
 class LRUCache {
+    // node of doubly linked list
     class Node{
         int key,value;
         Node next,prev;
@@ -7,10 +8,14 @@ class LRUCache {
             this.value = value;
         }
     }
+    // remove a node from linked list
     public void remove(Node root){
         root.prev.next = root.next;
         root.next.prev = root.prev;
     }
+
+    // add node just after head
+    // (mark as Most Recently Used)
     public void add(Node root){
 
         root.next = head.next;
@@ -28,7 +33,7 @@ class LRUCache {
         this.capacity = capacity;
         map = new HashMap<>();
 
-
+        // dummy head and tail nodes
         head = new Node(-1, -1);
         tail = new Node(-1, -1);
 
@@ -42,28 +47,34 @@ class LRUCache {
         }
 
         Node curr = map.get(key);
+        // move node to front (MRU)
         remove(curr);
         add(curr);
         return curr.value;
     }
     
     public void put(int key, int value) {
+        // key already exists
         if(map.containsKey(key)){
             Node curr = map.get(key);
+            // update value
             curr.value = value;
 
+            // move to front (MRU)
             remove(curr);
             add(curr);
             return;
         }
-
+        // cache full
         if(map.size() == capacity){
+            // remove Least Recently Used node
             Node lru = tail.prev;
 
             remove(lru);
             map.remove(lru.key);
         }
         Node newNode = new Node(key , value);
+        // add as Most Recently Used
         add(newNode);
         map.put(key , newNode);
     }
