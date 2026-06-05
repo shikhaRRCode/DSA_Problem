d2bb76c28b15a3fd3a6df18b787da60fcc1bb3db
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-06-05 20:28:47 +0530

    Time: 845 ms (5.00%) | Memory: 131.9 MB (38.89%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,4 +1,5 @@
 class LRUCache {
+    // stores {key, value} pairs
     ArrayList<int[]> cache = new ArrayList<>();
     int n;
     public LRUCache(int capacity) {
@@ -6,29 +7,41 @@ class LRUCache {
     }
 
     public int get(int key) {
+        // search key in cache
         for(int i = 0 ; i < cache.size() ; i++){
+            // key found
             if(cache.get(i)[0] == key){
+                // make it most recently used
                 int[] temp = new int[]{cache.get(i)[0] , cache.get(i)[1]};
                 cache.remove(i);
                 cache.add(temp);
+
+                // return value
                 return temp[1];
             }
         }
+        // key not present
         return -1;
     }
     
     public void put(int key, int value) {
         for(int i = 0 ; i < cache.size() ; i++){
+            // if key already exists
             if(cache.get(i)[0] == key){
+                // remove old entry
                 cache.remove(i);
+
+                // add updated entry at end
+                // (most recently used)
                 cache.add(new int[]{key,value});
                 return;
             }
         }
-
+        // cache full -> remove least recently used item
         if(cache.size() == n){
             cache.remove(0);
         }
+        // add new item as most recently used
         cache.add(new int[]{key , value});
     }
 }
