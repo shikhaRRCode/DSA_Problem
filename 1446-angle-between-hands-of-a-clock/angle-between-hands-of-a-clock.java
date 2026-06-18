8ac47a253379eecd835668d55f301129dcf894b0
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-06-18 19:40:11 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (76.92%) - LeetSync

--- 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.java
+++ 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.java
@@ -1,21 +1,38 @@
 class Solution {
     public double angleClock(int hour, int minutes) {
+
+        // minute hand moves 6° every minute
         double minAngle = minutes * 6;
+
+        // hour hand moves 30° every hour
         double hourAngle = hour * 30;
+
+        // hour hand also moves 0.5°(1/12) every minute
         double relation = minAngle / 12;
 
+        // angle between hour and minute hands
         double angle1 = Math.abs(minAngle - hourAngle - relation);
+
+        // angle in the opposite direction
         double angle2 = 360 - angle1;
 
-        return Math.min(angle1 , angle2); 
+        // return the smaller angle
+        return Math.min(angle1, angle2);
     }
 }
-// 1 min = 6 deg
-// 5 min = 30 deg
-// 1 hr = 30 deg
 
-// 60 min = 1 hr
-// 360 deg = 30 deg
-// 1deg = 30 % 360 = 1/12
+/*
+Logic:
 
-// 3 * 30 = 90, 180
+1 min  = 6°
+1 hour = 30°
+
+Hour hand also moves with minutes:
+60 min = 1 hr
+360 deg -> 30°
+1 min  -> 0.5° (= 1/12 of minute hand angle)/-
+
+Find the absolute angle between both hands,
+then return the smaller of:
+angle and (360 - angle).
+*/
