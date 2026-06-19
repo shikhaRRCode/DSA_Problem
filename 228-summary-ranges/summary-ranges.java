b56633685c1dd41edd29720630a36d23da3488e3
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-06-19 20:18:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (84.52%) - LeetSync

--- 228-summary-ranges/summary-ranges.java
+++ 228-summary-ranges/summary-ranges.java
@@ -5,6 +5,7 @@ class Solution {
 
         int i = 0;
         while(i < n){
+            // find the end of the current consecutive range
             int j = i+1;
             while(j < n){
                 if((nums[j-1]+1) != nums[j]){
@@ -12,12 +13,15 @@ class Solution {
                 }
                 j++;
             }
+            // build the range string
             StringBuilder sb = new StringBuilder();
             sb.append(nums[i]);
+            // if range has more than one element, append "->end"
             if(j-1 != i){
                 sb.append("->");
                 sb.append(nums[j-1]);
             }
+            // move to the next range
             i = j;
             ans.add(sb.toString());
         }
