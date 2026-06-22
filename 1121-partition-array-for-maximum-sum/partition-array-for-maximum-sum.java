7a73317f8ade26474f3d53f041a14404d91edd49
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-06-22 20:48:48 +0530

    Time: 6 ms (95.80%) | Memory: 44 MB (27.40%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -1,12 +1,15 @@
 class Solution {
     int n;
+    // DP with memoization
     int dp[] = new int[501];
     public int maxSumAfterPartitioning(int[] arr, int k) {
         n = arr.length;
         Arrays.fill(dp , -1);
+
         return solve(0 , arr , k); 
     }
     public int solve(int i , int[] arr , int k){
+        //Base case
         if(i == n){
             return 0;
         }
@@ -17,6 +20,7 @@ class Solution {
 
         int max = -1;
         int result= 0;
+        // At each index, explore partitions of size 1 to k
         for(int j = i ; j < n && j-i+1 <= k ; j++){
             max = Math.max(max , arr[j]);
 
