2731d69d714b04900a2c841b915bcf2950544caa
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-07-06 20:23:31 +0530

    Time: 7 ms (52.31%) | Memory: 46.6 MB (43.52%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.java
+++ 1222-remove-covered-intervals/remove-covered-intervals.java
@@ -1,14 +1,20 @@
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
         int n = intervals.length;
+        // Sort by start point ascending. 
+        // If start points are equal, sort by end point descending so the larger interval comes first.
         Arrays.sort(intervals , (a,b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
 
         List<int[]> temp = new ArrayList<>();
         temp.add(intervals[0]);
         for(int i = 1 ; i < n ; i++){
+            // If the current interval's end extends beyond the max end seen so far, 
+            // it is not completely covered, so we keep it.
             if(intervals[i][1] > temp.get(temp.size()-1)[1]){
                 temp.add(intervals[i]);
             }
+
+            // Otherwise, it is covered by a previous interval and safely skipped.
         }
         return temp.size();
     }
