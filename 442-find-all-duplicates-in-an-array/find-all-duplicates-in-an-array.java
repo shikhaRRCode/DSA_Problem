c27ebae58780459a3f36f3afd66a57424655a011
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-07-10 20:05:27 +0530

    Time: 6 ms (90.20%) | Memory: 57.6 MB (72.04%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -3,14 +3,23 @@ class Solution {
         int n = nums.length;
 
         List<Integer> ans = new ArrayList<>();
-        HashSet<Integer> set = new HashSet<>();
         for(int i = 0 ; i < n ; i++){
-            if(set.contains(nums[i])){
-                ans.add(nums[i]);
-            }
-            set.add(nums[i]);
-        }   
+            // Get the index that this value maps to
+            int index = Math.abs(nums[i]) - 1;
 
-        return ans;
+            // If the value at that index is already negative, it's a duplicate
+            if(nums[index] < 0){
+                ans.add(Math.abs(nums[i]));
+            }
+            else{
+                // Otherwise, mark it as visited by negating it
+                nums[index] = -nums[index];
+            }
+        } 
+        return ans;  
+
     }
 }
+// 0   1   2    3   4   5   6   7
+//{4 , 3 , 3 , -7 , 8 , 2 , 3 , 1}
+//
