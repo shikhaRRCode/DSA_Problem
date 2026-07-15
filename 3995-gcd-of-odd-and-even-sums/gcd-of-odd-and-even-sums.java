24946adb760551db367803db18fad88913585541
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-07-15 19:56:19 +0530

    Time: 1 ms (83.11%) | Memory: 42.1 MB (96.78%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -11,12 +11,13 @@ class Solution {
             odd+=2;even+=2;
         }
 
-        int ans = 1;
-        for(int i = 1 ; i < Math.min(sumOdd , sumEven) ; i++){
-            if(sumOdd % i == 0 && sumEven % i == 0){
-                ans = i;
-            }
+        return GCD(sumOdd , sumEven);
+    }
+    public int GCD(int a , int b){
+        if(b == 0){
+            return a;
         }
-        return ans;
+
+        return GCD(b , a%b);
     }
 }
