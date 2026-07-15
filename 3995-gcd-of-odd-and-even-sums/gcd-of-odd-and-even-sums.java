6706919691a03497e5d8c44d12fa015d50cd920a
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-07-15 20:01:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (98.16%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,23 +1,9 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int sumOdd = 0;
-        int sumEven = 0;
+        //sum of first n odd no. = n*n
+        //sum of first n even no. = n(n+1)
+        //therefore GCD of(n^2 , n^2 + n) is n
 
-        int odd = 1, even = 2;
-        for(int i = 1 ; i <= n ; i++){
-            sumOdd+=odd;
-            sumEven+=even;
-
-            odd+=2;even+=2;
-        }
-
-        return GCD(sumOdd , sumEven);
-    }
-    public int GCD(int a , int b){
-        if(b == 0){
-            return a;
-        }
-
-        return GCD(b , a%b);
+        return n;
     }
 }
