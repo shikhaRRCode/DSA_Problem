6d21be2614c9954066301ec040215f592dc1993c
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-08-11 16:35:55 +0530

    Time: 43 ms (25.20%) | Memory: 49.4 MB (45.93%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.java
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -4,7 +4,7 @@ class Solution {
         t = new int[n+1];
         Arrays.fill(t , -1);
 
-        return solve(n); 
+        return solve(n);   //Alice k lie call hai ye. If it's true, Alice wins, else Alice looses
     }
     public boolean solve(int n){
         if(n == 0){
@@ -15,13 +15,16 @@ class Solution {
             return t[n] == 0 ? false : true;
         }
 
-        for(int k = 1 ; k*k <= n ; k++){
-            if(solve(n-k*k) == false){
+        for(int k = 1 ; k*k <= n ; k++){  
+            
+            if(solve(n-k*k) == false){  //Call for Bob //False - Bob lost it
+                //Alice won the game
                 t[n] = 1;
                 return true;
             }
         }   
         t[n] = 0;
-        return false;
+        return false;    //Alice could never win. Lost it.
     }
-}
+} //T.C : O(n * sqrt(n))
+//S.C : O(n)
