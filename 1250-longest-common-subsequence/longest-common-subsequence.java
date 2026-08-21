3fe17c86b2e4e51ba33205e4fccd2c6d776e537e
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-08-21 19:55:32 +0530

    Time: 19 ms (91.83%) | Memory: 53.4 MB (90.20%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,28 +1,37 @@
 class Solution {
     int[][] dp;
-    public int longestCommonSubsequence(String text1, String text2) {
-        int n = text1.length();
-        int m = text2.length();
+    public int longestCommonSubsequence(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
         dp = new int[n+1][m+1];
-        for(int[] arr : dp){
-            Arrays.fill(arr , -1);
-        }
 
-        return solve(text1 , text2 , n , m);
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= m ; j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
+                }
+            }
+        }
+    
+        return dp[n][m];
     }
-    public int solve(String s1 , String s2 , int n , int m){
-        if(n == 0 || m == 0){
-            return 0;
-        }
+    // public int solve(String s1 , String s2 , int n , int m){
+    //     if(n == 0 || m == 0){
+    //         return 0;
+    //     }
 
-        if(dp[n][m] != -1){
-            return dp[n][m];
-        }
+    //     if(dp[n][m] != -1){
+    //         return dp[n][m];
+    //     }
 
-        if(s1.charAt(n-1) == s2.charAt(m-1)){
-            return dp[n][m] =  1 + solve(s1 , s2 , n-1 , m-1);
-        }
+    //     if(s1.charAt(n-1) == s2.charAt(m-1)){
+    //         return dp[n][m] =  1 + solve(s1 , s2 , n-1 , m-1);
+    //     }
         
-        return dp[n][m] = Math.max(solve(s1 , s2 , n-1 , m) , solve(s1 , s2 , n , m-1));  
-    }
+    //     return dp[n][m] = Math.max(solve(s1 , s2 , n-1 , m) , solve(s1 , s2 , n , m-1));  
+    // }
 }
+//STATE DEFINITION: dp[i][j] = longest common subsequence between s1 of length i and s2 of length j.
