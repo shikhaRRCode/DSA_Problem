c2b52ce3829a23d3df7fdf1776619ee06b37c229
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-08-28 11:55:33 +0530

    Time: 32 ms (24.32%) | Memory: 52.6 MB (8.98%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,10 +1,17 @@
 class Solution {
+    int[][] dp;
     public int countSubstrings(String s) 
     {
+        int n = s.length();
+        dp = new int[n+1][n+1];
+        for(int[] arr : dp){
+            Arrays.fill(arr , -1);
+        }
+
         int count=0;
-        for(int i=0 ; i<s.length() ; i++)
+        for(int i = 0 ; i < n ; i++)
         {
-            for(int j=i; j < s.length() ; j++)
+            for(int j = i; j < n ; j++)
             {
                 if(ispalindromic(s , i , j))
                 {
@@ -14,14 +21,20 @@ class Solution {
         }
         return count;
     }
-    public static boolean ispalindromic(String s , int i , int j)
+    public boolean ispalindromic(String s , int i , int j)
     {
         if(i >= j){
             return true;
         }
 
+        if(dp[i][j] != -1){
+            return dp[i][j] == 1 ? true : false;
+        }
+
         if(s.charAt(i) == s.charAt(j)){
-            return ispalindromic(s , i+1 , j-1);
+            boolean check = ispalindromic(s , i+1 , j-1);
+            dp[i][j] = check ? 1 : 0;
+            return check;
         }
         return false;
     }    
