92927c2cdd6aa0ecae2b0d040fd3bd6488241b62
shikhaRRCode <shikharchoudhary0987@gmail.com>
2026-09-09 19:50:23 +0530

    Time: 1 ms (83.58%) | Memory: 42.8 MB (8.36%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,9 +1,5 @@
 class Solution {
     public int countCommas(int n) {
-        int count = 0;
-        for(int threshHold = 1000 ; threshHold <= n ; threshHold*=1000){
-            count += n - threshHold + 1;
-        }
-        return count;
+        return Math.max(0 , n-1000+1);
     }
 }
